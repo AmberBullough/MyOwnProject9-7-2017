71d7969c3400dc07f5d29109f500cf7328d99908
Amber Bullough <amber@bertbullough.com>
2017-09-07 08:58:36 -0600

    Added in .gitignore and added reposityory

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,11 @@
+package stupid.controller;
+
+public class StupidController {
+	
+	public void start()
+		{
+			System.out.println("What is the meaning of life? 42. How much wood can a woodchuck chuck if a woodchuck could chuck wood? Blah I don't know.");
+		}
+	}
+
+
