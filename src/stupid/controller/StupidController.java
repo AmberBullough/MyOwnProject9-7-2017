3bc3cf312f7822ebf5ec522214ec6255c74ba881
Amber Bullough <amber@bertbullough.com>
2017-09-07 09:00:02 -0600

    Changed Text in controller

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -4,7 +4,10 @@ public class StupidController {
 	
 	public void start()
 		{
-			System.out.println("What is the meaning of life? 42. How much wood can a woodchuck chuck if a woodchuck could chuck wood? Blah I don't know.");
+			System.out.println("What is the meaning of life? "
+					+ "42. "
+					+ "How much wood can a woodchuck chuck if a woodchuck could chuck wood? "
+					+ "Blah I don't know. Ask the woodchuck.");
 		}
 	}
 
