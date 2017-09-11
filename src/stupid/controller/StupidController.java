fb262cac3e8143db827d52bb900deebf9f437a4b
Amber Bullough <amber@bertbullough.com>
2017-09-11 07:59:58 -0600

    Deleted some lines of text

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -8,14 +8,7 @@ public class StupidController {
 		{
 			System.out.println("What is the meaning of life? ");
 			System.out.println("42.");
-			System.out.println("How much wood can a woodchuck chuck if a woodchuck could chuck wood? ");
-			System.out.println("Blah I don't know. Ask the woodchuck.");
-		}
-	{
-			System.out.println ("What is this feeling? I feel my heart hammering in my chest. Where has my breath gone?");
-			System.out.println("Struggling to breathe. What is this feeling? I feel dizzy. Where is my mind? I cannot think.");
-			System.out.println("What is this feeling? A sense of loneliness. Where is my sanity? Is this love or am I dying?");
-			testScanner();
+	
 	}
 
 	
